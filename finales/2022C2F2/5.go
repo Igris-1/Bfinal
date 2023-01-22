2babfd6a76d25cf902d7f633f6bea246342ca5ab
Igris-1 <marcelo_n97@outlook.es>
2023-01-22 17:59:55 -0300

    Update ejercicio de final 2022C2F2

--- finales/2022C2F2/5.go
+++ finales/2022C2F2/5.go
@@ -1,9 +1,24 @@
 package ejercicio5
 
+import (
+	hash "material/resources/hash"
+)
+
 /*
 5. Implementar un algoritmo que reciba un arreglo desordenado de enteros,
 su largo (n) y un número K y determinar en O(n) si existe un par de elementos
 en el arreglo que sumen exactamente K.
 */
 
-// TODO: terminar
+func SumaK(arr []int, n, k int) bool {
+	hash := hash.CrearHash[int, bool]()
+
+	for i := 0; i < n; i++ {
+		elemento := (k - arr[i])
+		if hash.Pertenece(elemento) {
+			return true
+		}
+		hash.Guardar(arr[i], true)
+	}
+	return false
+}
