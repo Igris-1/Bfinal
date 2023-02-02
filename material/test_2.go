27b08456ca5c06919432584468c4bb91b53e3952
Nicolas <marcelo_n97@outlook.es>
2023-02-02 20:44:17 -0300

    Archivos test

--- material/test_2.go
+++ material/test_2.go
@@ -3,19 +3,19 @@ package main
 import "strconv"
 
 func countingSort(arr []int) int {
-	count := make([]int, 10)
-	out := make([]int, len(arr))
-	for i := 0; i < len(arr); i++ {
-		count[arr[i]]++
+	count := make([]int, 10) // creo un arreglo con el rango acotado
+	out := make([]int, len(arr)) // creo un arreglo del rango del arreglo original
+	for i := 0; i < len(arr); i++ { // recorro el len() del arreglo original
+		count[arr[i]]++ // aumento el contador del arreglo acotado
 	}
-	for i := 1; i < len(count); i++ {
-		count[i] += count[i-1]
+	for i := 1; i < len(count); i++ { // recorro el len() del arreglo acotado
+		count[i] += count[i-1] // sumo el contador del arreglo acotado
 	}
-	for i := len(arr) - 1; i >= 0; i-- {
-		out[count[arr[i]]-1] = arr[i]
-		count[arr[i]]--
+	for i := 0; i < len(arr); i++ { // recorro el len() del arreglo original
+		out[count[arr[i]]-1] = arr[i] // asigno el valor del arreglo original al arreglo acotado
+		count[arr[i]]-- // disminuyo el contador del arreglo acotado
 	}
-	return arreglo_a_numero(out)
+	return arreglo_a_numero(out) // retorno el arreglo acotado convertido a numero
 }
 
 func arreglo_a_numero(arr []int) int {
